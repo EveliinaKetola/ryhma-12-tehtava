9a164f13f393d77f6206c8b4519a7e47840069d3
Eveliina Ketola <eveliina.ketola@elisanet.fi>
2025-11-13 19:58:37 +0200

    Oman nimen lisäys

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Tiimiimme kuuluu:");
+        System.out.println("Tiimiimme kuuluu: Eveliina");
     }
 }
